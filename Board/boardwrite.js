a0518777c30d1f60e8f4300ec9573672de7d40f3
istjprogrammer <139939895+istjprogrammer@users.noreply.github.com>
2023-12-14 17:23:31 +0900

    [리엑트] 게시판 글 쓰기

---
+++ Board/boardwrite.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Quill 스타일을 불러옴
+import FileUpload from './fileupload.js';
+
+const BoardWrite = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('전체글');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleCategoryChange = (selected) => {
+    setSelectedCategory(selected);
+  };
+
+  const handleUpdate = () => {
+    setShowAlert(true);
+    // 여기에서 등록 로직을 추가할 수 있어
+  };  
+
+  return (
+    <Container>
+    <main className="mt-5 pt-5">
+      <Row className="justify-content-center">
+        <Col className="text-center">
+          <h1>글 등록</h1>
+        </Col>
+      </Row>
+      <hr/>
+      <br></br><br></br>
+      <Form>                    
+          <InputGroup>
+            <InputGroup.Text style={{ width: '100px', fontWeight: 'bold' }}>카테고리</InputGroup.Text>
+            <Form.Select value={selectedCategory} onChange={(e) => handleCategoryChange(e.target.value)}>
+              <option value="구인/구직">구인/구직</option>
+              <option value="이직/신입">이직/신입</option>
+              <option value="면접">면접</option>
+              <option value="자소서">자소서</option>
+              <option value="합격자조언">합격자조언</option>
+            </Form.Select><br></br>
+          </InputGroup> 
+
+            <InputGroup>
+              <InputGroup.Text style={{ width: '100px', fontWeight: 'bold' }}>제목</InputGroup.Text>
+              <Form.Control type="text" value={title} onChange={handleTitleChange} />              
+            </InputGroup>
+
+        <FileUpload />
+
+        <Form.Group className="mb-4">
+          <ReactQuill
+            style={{ height: '400px' }}
+            theme="snow" value={content} onChange={setContent} />
+        </Form.Group>
+
+        <Row className="justify-content-start">
+          <Col>
+            <div className="d-flex justify-content-end mt-3">
+              {/* Check if showAlert is true, and display the alert */}
+              {showAlert && (
+                <div className="alert alert-success mr-3" role="alert">
+                  등록이 완료되었습니다!
+                </div>
+              )}
+
+              {/* Use a button to trigger the update and show the alert */}
+              <div className="d-flex justify-content-end mt-3">
+                <Link to='/'>
+                  <Button variant="primary" onClick={handleUpdate}>
+                    등록
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Form>
+      </main>
+    </Container>
+  );
+};
+
+export default BoardWrite;
