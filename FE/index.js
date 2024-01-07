457f6131b18189aaf607452e716b1aacae3cff6c
istjprogrammer <139939895+istjprogrammer@users.noreply.github.com>
2024-01-07 22:47:37 +0900

    index.js : 조회수 2회 증가 이슈 해결

---
+++ FE/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  //   <BrowserRouter>
+  //     <Header/>
+  //     <App />
+  //     <Footer/>
+  //   </BrowserRouter>
+  // </React.StrictMode>
+
+  <BrowserRouter>
+    <Header/>
+    <App />
+    <Footer/>
+  </BrowserRouter>
+);
